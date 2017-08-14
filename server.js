c963324574b498b1241808482b8b2a59a5b52afd
ManojSandoori <manoj.sandoori@gmail.com>
2017-08-14 18:40:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,10 +110,10 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function (req,res) {
+app.get('/submit-name', function (req,res) { // URL : /submit-name?name=xxxxx
     // Get the name from request
     
-    var name = req.params.name; // TODO
+    var name = req.query.name; // TODO
     names.push(name);
     // JSON - Java Script Object Notation
     res.send(JSON.stringify(names)); // TODO
