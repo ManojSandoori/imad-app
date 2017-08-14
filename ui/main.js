47cbda83d5a99e9a4b77665f2a48cfd74fec1066
ManojSandoori <manoj.sandoori@gmail.com>
2017-08-14 16:13:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,12 @@ element.innerHTML = 'My name is Madi FYI';
 
 //move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+    function moveRight (){
+        marginLeft = marginLeft + 1;
+        img.style.marginLeft = marginLeft + 'px';
+    }
 img.onclick = function (){
-    var interval = setInterval(moveLeft, 50);
+    var interval = setInterval(moveRight, 50);
    
 };
