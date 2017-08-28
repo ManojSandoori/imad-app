3dbbd971df1b40ff0cf9a1954e3a1abb44eeae39
ManojSandoori <manoj.sandoori@gmail.com>
2017-08-28 18:52:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user:'manojsandoori',
@@ -14,7 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 
 function createTemplate (data) {
     var title = data.title;
@@ -69,6 +70,26 @@ app.get('/hash/:input', function (req, res) {
     var hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
     
+});
+
+app.post('/create-user', function (req,res) {
+    // username, password
+    // {"username" :"manojsandoori", "password": "password"}
+    // JSON
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    pool.query('INSERT into "user"(username,password) VALUES ($1,$2)',[username, dbString], function (err,result) {
+        
+        if (err) {
+           res.status(500).send(err.toString());
+       } 
+       else {
+           res.send('User successfully created:' + username);
+       }
+        
+    });
 });
 
 var pool = new Pool(config);
