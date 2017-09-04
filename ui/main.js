8265a5b283d66a80fd32d2c713eba6fb9c6e2a36
ManojSandoori <manoj.sandoori@gmail.com>
2017-09-04 15:02:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,28 +12,26 @@ request.onreadystatechange = function (){
           if (request.readyState === XMLHttpRequest.DONE) 
           {
               //Take Some Action
-              if (request.status === 200) 
-                { 
+              if (request.status === 200) { 
                     // Capture a list of names and render it as a list
-                    var names = request.responseText;
-                    names = JSON.parse(names);
-                    var list = '';
-                    for( var i=0; i<names.length; i++ ){
-                        
-                        list += '<li>' + names[i] + '</li>';
-                        
-                    }
-                    var ul = document.getElementById('namelist');
-                    ul.innerHTML = list;
-                } 
+                   console.log('User logged in');
+                   alert('Logged in successfully');
+                } else if(request.status === 403) {
+                    alert('Username/password is incorrect');
+                } else if(request.status === 500) {
+                    alert('Something went wrong on the server');
+                }
           } 
           // Not done yet
     }; 
     
     // Make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open("GET", 'http://manojsandoori.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null); 
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://manojsandoori.imad.hasura-app.io/login' + name, true);
+    request.setRequestHeader('Content-type','application/json');
+    request.send(JSON.stringify({username: username, password: password})); 
     
 };
